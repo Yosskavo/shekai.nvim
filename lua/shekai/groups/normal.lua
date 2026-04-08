8a80b396bd5fc31fc3abb17a772f91951c788ccc
yosskavo <yassutosheaki@gmail.com>
2026-04-08 20:21:34 +0100

    test

---
+++ lua/shekai/groups/normal.lua
@@ -0,0 +1,14 @@
+local p = require("shekai.colors").my_colors
+
+local m = {
+	Normal			= {fg = p.blue_sky, bg = p.bg_alt},
+	NormalNC		= {fg = p.blue_cyan, bg = p.bg},
+	NormalFloat		= {link = "Normal"},
+	SignColumn		= {link = "Normal"},
+	Pmenu			= {fg = p.fg_muted, bg = p.bg_dim},
+    PmenuSel		= {fg = p.silver, bg = p.visual},
+    PmenuSbar		= {fg = p.fg_soft, link = "Pmenu"},
+    PmenuThumb		= {fg = p.fg_faint, "Pmenu"},
+}
+
+return m
