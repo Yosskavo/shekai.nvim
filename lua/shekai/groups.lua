e300bcc750ed9227222bb0dc6d23f10cbd898cd8
yosskavo <yassutosheaki@gmail.com>
2026-04-08 20:29:17 +0100

    test

--- lua/shekai/groups.lua
+++ lua/shekai/groups.lua
@@ -1,3 +1,18 @@
-return  {
-	require("shekai.groups.normal")
+-- lua/shekai/groups/init.lua
+
+local all_highlights = {}
+
+-- Just list the names of the files in your 'groups' folder here
+local modules = {
+	"normal"
 }
+
+for _, mod in ipairs(modules) do
+    -- Dynamically require each file
+    local module_highlights = require("shekai.groups." .. mod)
+    
+    -- Merge it into the main table
+    all_highlights = vim.tbl_extend("force", all_highlights, module_highlights)
+end
+
+return all_highlights
