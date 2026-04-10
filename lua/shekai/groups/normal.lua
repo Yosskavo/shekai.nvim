7e52f3dc9d7a450359518bf9f996a945a478a817
yosskavo <yassutosheaki@gmail.com>
2026-04-10 05:37:57 +0100

    test

--- lua/shekai/groups/normal.lua
+++ lua/shekai/groups/normal.lua
@@ -6,7 +6,7 @@ local m = {
 	NormalFloat		= {link = "Normal"},
 	SignColumn		= {link = "Normal"},
 	Pmenu			= {fg = p.fg_muted, bg = p.bg_dim},
-    PmenuSel		= {fg = p.silver, bg = p.tan},
+    PmenuSel		= {fg = p.gold, bg = p.blue_accent},
     PmenuSbar		= {fg = p.fg_soft, link = "Pmenu"},
     PmenuThumb		= {fg = p.fg_faint, link = "Pmenu"},
 }
