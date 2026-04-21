15804c7de528943b5d63ed3ceb6a83eb0b951322
Yassine El motaouakkel <yel-mota@c1r2p1.1337.ma>
2026-04-21 09:58:02 +0100

    diago

--- lua/shekai/groups/diagnostic.lua
+++ lua/shekai/groups/diagnostic.lua
@@ -2,11 +2,11 @@ local p = require("shekai.colors").my_colors
 
 local m =
 {
-	DiagnosticError = { fg = p.deep_blood }, -- Virtual text/signs for syntax errors
+	DiagnosticError = { fg = p.red }, -- Virtual text/signs for syntax errors
     DiagnosticWarn  = { fg = p.light_yellow }, -- Virtual text/signs for warnings
     DiagnosticInfo  = { fg = p.light_blue }, -- Virtual text/signs for general info
     DiagnosticHint  = { fg = p.light_green }, -- Virtual text/signs for helpful hints
-    DiagnosticUnderlineError    = { fg = p.deep_blood, undercurl = true, bold = true }, -- Wavy underline for errors
+    DiagnosticUnderlineError    = { fg = p.red, undercurl = true, bold = true }, -- Wavy underline for errors
     DiagnosticUnderlineWarn     = { fg = p.light_yellow, undercurl = true }, -- Wavy underline for warnings
     DiagnosticUnderlineInfo     = { fg = p.light_blue, undercurl = true }, -- Wavy underline for info
     DiagnosticUnderlineHint     = { fg = p.light_green, undercurl = true }, -- Wavy underline for hints
